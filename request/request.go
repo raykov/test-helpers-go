e4d7522c96b2106447cef94a88f9a972f999408c
Andrii Raikov <andrii.raikov@savedo.de>
2019-10-08 15:06:30 +0200

    Add Request helpers

---
+++ request/request.go
@@ -0,0 +1,26 @@
+package request
+
+import "net/http"
+
+type Header struct {
+  Name string
+  Val string
+}
+
+func Get(url string, headers []Header) (req *http.Request) {
+  return request("GET", url, headers)
+}
+
+func Post(url string, headers []Header) (req *http.Request) {
+  return request("POST", url, headers)
+}
+
+func request(method, url string, headers []Header) (req *http.Request) {
+  req, _ = http.NewRequest(method, url, nil)
+
+  for _, h := range headers {
+    req.Header.Set(h.Name, h.Val)
+  }
+
+  return
+}
