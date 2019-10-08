59487b406b6b539292ce478ce9f14b4d6b709d86
Andrii Raikov <andrii.raikov@savedo.de>
2019-10-08 15:06:15 +0200

    Add Assert helpers

---
+++ assert/assert.go
@@ -0,0 +1,25 @@
+package assert
+
+import (
+  "testing"
+)
+
+func True(t *testing.T, result bool, testCase string) {
+  Boolean(t, result, true, testCase)
+}
+
+func False(t *testing.T, result bool, testCase string) {
+  Boolean(t, result, false, testCase)
+}
+
+func Boolean(t *testing.T, result, expected bool, testCase string) {
+  if result != expected {
+    t.Errorf("returned unexpected result for %v: \ngot:  `%v` \nwant: `%v`\n", testCase, result, expected)
+  }
+}
+
+func Equal(t *testing.T, expected, actual interface{}, testCase string) {
+  if expected != actual {
+    t.Errorf("returned unexpected result for %v: \ngot:  `%v` \nwant: `%v`\n", testCase, actual, expected)
+  }
+}
