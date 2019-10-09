f03c61c1b4c2f8d3fed38a7a9c402d97c9cc2694
Andrii Raikov <andrii.raikov@savedo.de>
2019-10-09 16:25:37 +0200

    Dont allow to reuse internal structs.
    Accept empty interface and try to convert it to []header. If not raise
    an error.

--- request/request.go
+++ request/request.go
@@ -2,23 +2,29 @@ package request
 
 import "net/http"
 
-type Header struct {
+type header struct {
 	Name string
 	Val string
 }
 
-func Get(url string, headers []Header) (req *http.Request) {
+func Get(url string, headers interface{}) (req *http.Request) {
 	return request("GET", url, headers)
 }
 
-func Post(url string, headers []Header) (req *http.Request) {
+func Post(url string, headers interface{}) (req *http.Request) {
 	return request("POST", url, headers)
 }
 
-func request(method, url string, headers []Header) (req *http.Request) {
+func request(method, url string, headers interface{}) (req *http.Request) {
+	var checkedHeaders []header
+	checkedHeaders, ok := headers.([]header)
+	if !ok {
+		panic("Headers should be an array of struct with Name and Val fields")
+	}
+
 	req, _ = http.NewRequest(method, url, nil)
 
-	for _, h := range headers {
+	for _, h := range checkedHeaders {
 		req.Header.Set(h.Name, h.Val)
 	}
 
